96b8927bbc4e8b122e97de184db0cfe81d2b0b00
JIAYI <4a50h037@stust.edu.tw>
2020-08-20 22:53:46 +0800

    Add code

--- app.py
+++ app.py
@@ -35,11 +35,12 @@ def callback():
 def handle_message(event):
     if event.message.text == "123":
         message = TextSendMessage(text="https://instagram.fkhh1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/117876972_141643294277090_4439974692272446771_n.jpg?_nc_ht=instagram.fkhh1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=txohWtXF16MAX-8A4pf&oh=234a359855f7ed1961e2f93d3cb2dfb5&oe=5F67ED3E")
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token, message,ImageSendMessage(original_content_url='https://instagram.fkhh1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/117876972_141643294277090_4439974692272446771_n.jpg?_nc_ht=instagram.fkhh1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=txohWtXF16MAX-8A4pf&oh=234a359855f7ed1961e2f93d3cb2dfb5&oe=5F67ED3E', preview_image_url='https://instagram.fkhh1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/117876972_141643294277090_4439974692272446771_n.jpg?_nc_ht=instagram.fkhh1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=txohWtXF16MAX-8A4pf&oh=234a359855f7ed1961e2f93d3cb2dfb5&oe=5F67ED3E'))
+        # line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='圖片網址', preview_image_url='圖片網址'))
     else:
         message = TextSendMessage(text=event.message.text)
-        line_bot_api.reply_message(event.reply_token, message)
-
+        line_bot_api.reply_message(event.reply_token, message,ImageSendMessage(original_content_url='https://instagram.fkhh1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/117876972_141643294277090_4439974692272446771_n.jpg?_nc_ht=instagram.fkhh1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=txohWtXF16MAX-8A4pf&oh=234a359855f7ed1961e2f93d3cb2dfb5&oe=5F67ED3E', preview_image_url='https://instagram.fkhh1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/117876972_141643294277090_4439974692272446771_n.jpg?_nc_ht=instagram.fkhh1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=txohWtXF16MAX-8A4pf&oh=234a359855f7ed1961e2f93d3cb2dfb5&oe=5F67ED3E'))
+        # line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='圖片網址', preview_image_url='圖片網址'))
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
