c3bcf402c1262a935c94c626bb4e1b56a35ebb12
JIAYI <4a50h037@stust.edu.tw>
2020-08-20 22:47:59 +0800

    Add code

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == "123":
-        message = TextSendMessage(text="我愛你")
+        message = TextSendMessage(text="https://instagram.fkhh1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/117876972_141643294277090_4439974692272446771_n.jpg?_nc_ht=instagram.fkhh1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=txohWtXF16MAX-8A4pf&oh=234a359855f7ed1961e2f93d3cb2dfb5&oe=5F67ED3E")
         line_bot_api.reply_message(event.reply_token, message)
     else:
         message = TextSendMessage(text=event.message.text)
